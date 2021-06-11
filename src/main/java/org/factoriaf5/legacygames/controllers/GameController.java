645a25f8aee68220417f538892f2ba68c1476e80
Marta SH <msastreharo@gmail.com>
2021-06-11 13:39:08 +0200

    Add done, edit in progress, delete pending. Current problem with edit form

--- src/main/java/org/factoriaf5/legacygames/controllers/GameController.java
+++ src/main/java/org/factoriaf5/legacygames/controllers/GameController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,9 +21,25 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @GetMapping("/new")
+    @GetMapping("games/new")
+    public String newGame(Model model) {
+        Game game = new Game();
+        model.addAttribute("game", game);
+        model.addAttribute("title", "Create a new game");
+        return "games/new";
+    }
+
+    @PostMapping("games/new")
     public String addGames(@ModelAttribute Game game) {
         gameService.save(game);
         return "redirect:/home";
+    }
+
+    @GetMapping("games/new/edit/{id}")
+    public String editGame(Model model, @PathVariable Long id) {
+        Game game = gameService.findById(id);
+        model.addAttribute("game", game);
+        model.addAttribute("title", "Edit game");
+        return "games/edit";
     }
 }
