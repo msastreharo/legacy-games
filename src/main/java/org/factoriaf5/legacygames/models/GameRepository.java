06370f55c752b647750d8286a6f47685f4597036
Marta SH <msastreharo@gmail.com>
2021-06-30 18:11:48 +0200

    First style layout done

--- src/main/java/org/factoriaf5/legacygames/models/GameRepository.java
+++ src/main/java/org/factoriaf5/legacygames/models/GameRepository.java
@@ -1,7 +1,8 @@
 package org.factoriaf5.legacygames.models;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends CrudRepository<Game, Long> { }
+public interface GameRepository extends JpaRepository<Game, Long> { }
